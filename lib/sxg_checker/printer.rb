e6cc862b9432885da105dbcf9821cddbb8838876
Pawel <pepawel@users.noreply.github.com>
2025-05-10 13:46:19 +0200

    Replace Formatter with Printer, display subresources in a more readable fashion

--- lib/sxg_checker/printer.rb
+++ lib/sxg_checker/printer.rb
@@ -1,13 +1,19 @@
 module SxgChecker
-  class Formatter
-    def resource(resource)
-      icon = icons.fetch(resource.status)
-      "#{icon} #{resource.url}"
+  class Printer
+    def call(document)
+      puts "#{icon(document)} #{document.url}"
+      document.subresources.sort.each do |subresource|
+        puts "#{icon(subresource)}   #{subresource.url}"
+      end
     end
 
     attr_reader :icons
 
     private
+
+    def icon(resource)
+      icons.fetch(resource.status)
+    end
 
     def initialize(icons = {
       ok: "✓",
